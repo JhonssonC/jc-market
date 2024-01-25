792cc66375afa9f4e70f5c800aa1be8a7fbc0a68
JhonssonC <ionszon1806@gmail.com>
2024-01-25 16:17:13 -0500

    Start

---
+++ src/main/java/com/jc/market/JcMarketApplication.java
@@ -0,0 +1,13 @@
+package com.jc.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jc/market/JcMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jc.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
